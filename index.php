7a543fbd3f67acde55c1dd65771470fc5ed46dc9
Praneeth Reddy <88426073+PenugondaPraneethReddy@users.noreply.github.com>
2023-05-13 19:28:02 +0530

    work completed

---
+++ index.php
@@ -0,0 +1,108 @@
+<?php include 'include/header.php'; ?>
+
+<!-- section 1 -->
+<div class="container mb-5 mt-5">
+  <div class="p-5 mb-4 text-bg-dark rounded-3">
+    <div class="d-flex flex-lg-nowrap flex-wrap text-center">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold ">Automated Book Finder using Raspberry PI</h1>
+        <p class="fs-4">
+          Find a Book with Light using raspberry PI
+        </p>
+      </div>
+      <div class="container-fluid">
+        <img src="assets/images/pi.jpg" alt="pi-img" class="img-fluid rounded-5" height="250rem" width="250rem">
+      </div>
+    </div>
+  </div>
+
+  <div id="about" class="container-fluid mt-5">
+    <div class="row">
+      <div class="col-sm-12" align="justify">
+        <h2>About Project</h2><br>
+        <p>In this project, a website that assists in finding the desired book will be developed. HTML, Bootstrap, PHP,
+          jQuery, JavaScript, and Django will be used to build the website. To find a reference number, a database is
+          searched for the book's title. The LED strips show the user the precise location of the book inside the shelf
+          using a Raspberry Pi.
+          This project will assist users in searching the books quickly with mobile devices and smartphones and can look
+          for the necessary books without even touching the racks, as the
+          technologies used previously for finding or searching the books arise some problems in searching books.</p>
+        <br><button class="btn btn-dark btn-lg" onclick="window.location.href = 'https:github.com/PenugondaPraneethReddy/Book-Self-Finder'">Project link <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-5">
+    <h1 class="mb-3">Project Team</h1>
+    <div class="d-flex flex-lg-nowrap flex-md-nowrap flex-wrap">
+      <div class="col-md-4 me-3">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top"
+            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+            alt="Thumbnail [100%x225]" style="height: 350px; width: 100%; display: block;"
+            src="assets/images/Praneeth.jpeg"
+            data-holder-rendered="true">
+          <div class="card-body">
+          <h4>P Praneeth Reddy (19BCI0154)</h4>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-person"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-instagram"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-linkedin"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-github"></i></button>
+              </div>
+              <small class="text-muted"></small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 me-3">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top"
+            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+            alt="Thumbnail [100%x225]" style="height: 350px; width: 100%; display: block;"
+            src="assets/images/satya.jpeg"
+            data-holder-rendered="true">
+          <div class="card-body">
+            <h4>K Venkata Satya (19BCI0142)</h4>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-person"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-instagram"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-linkedin"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-github"></i></button>
+              </div>
+              <small class="text-muted"></small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 me-3">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top"
+            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+            alt="Thumbnail [100%x225]" style="height: 350px; width: 100%; display: block;"
+            src="assets/images/RadhaKrishna.jpeg"
+            data-holder-rendered="true">
+          <div class="card-body">
+          <h4>B Radha Krishna (19BCT0255)</h4>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-person"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-instagram"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-linkedin"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-github"></i></button>
+                
+              </div>
+              <small class="text-muted"></small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<?php include 'include/footer.php' ?>
